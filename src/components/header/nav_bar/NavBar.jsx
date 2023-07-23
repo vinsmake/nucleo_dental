6744636b9125c98e0ad2df5ebd042262accd3bf9
vinsmake <ps.enrique.e@gmail.com>
2023-07-22 18:35:11 -0600

    Project updated

--- src/components/header/nav_bar/NavBar.jsx
+++ src/components/header/nav_bar/NavBar.jsx
@@ -4,30 +4,30 @@ export const NavBar = () => {
     return (
         <>
             <section className='navBar'>
-                <nav className='navBar_bar'>
-                    <a href='#' className='navBar_bar--logo'>
+                <nav className='navBar__bar'>
+                    <a href='#' className='navBar__bar--logo'>
                         Núcleo Dental
                     </a>
 
-                    <div className='navBar_bar__links'>
-                        <ul className='navBar_bar__links__list'>
+                    <div className='navBar__bar__links'>
+                        <ul className='navBar__bar__links__list'>
                             <li>
-                                <a className='navBar_bar__links__list--link' href='#inicio'> Inicio </a>
+                                <a className='navBar__bar__links__list--link' href='#inicio'> Inicio </a>
                             </li>
                             <li>
-                                <a className='navBar_bar__links__list--link' href='#servicios'> Servicios </a>
+                                <a className='navBar__bar__links__list--link' href='#servicios'> Servicios </a>
 
                             </li>
                             <li>
-                                <a className='navBar_bar__links__list--link' href='#tratamientos'> Tratamientos </a>
+                                <a className='navBar__bar__links__list--link' href='#tratamientos'> Tratamientos </a>
                             </li>
                             <li>
-                                <a className='navBar_bar__links__list--link' href='#nosotros'> Nosotros </a>
+                                <a className='navBar__bar__links__list--link' href='#nosotros'> Nosotros </a>
                             </li>
                         </ul>
                     </div>
 
-                    <a className='navBar_bar--whats' href="https://api.whatsapp.com/send?phone=523329147808&text=%C2%A1Hola%20N%C3%BAcleo%20Dental!">
+                    <a className='navBar__bar--whats' href="https://api.whatsapp.com/send?phone=523329147808&text=%C2%A1Hola%20N%C3%BAcleo%20Dental!">
                         Enviar whatsapp
                     </a>
 
