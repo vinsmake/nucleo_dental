baea6f47d6a5084b2284661d49825f5eb5694a90
vinsmake <ps.enrique.e@gmail.com>
2023-08-26 17:23:32 -0600

    Project updated

--- src/data.js
+++ src/data.js
@@ -1,12 +1,19 @@
 export const headerData = {
+    barData: {
+        facebook: {href: 'https://www.facebook.com/proatencionmedica'},
+        whatsapp: {href: 'https://api.whatsapp.com/send?phone=523329147808&text=%C2%A1Hola%20N%C3%BAcleo%20Dental!', text: '+52 1 33 2914 7808'},
+        instagram: {href: 'https://www.instagram.com/nucleodental.mzo/'},
+        tel: {href: '3329147808'}
+
+    },
     navData: {
-    navName: {href: '#', text: 'Núcleo Dental'},
-    navLinks: [
-        { href: '#home', text: 'Inicio' },
-        { href: '#services', text: 'Servicios' },
-        { href: '#specialties', text: 'Especialidades' },
-        { href: '#nosotros', text: 'Nosotros' },
-    ],
-    navBtn: {href: 'https://api.whatsapp.com/send?phone=523329147808&text=%C2%A1Hola%20N%C3%BAcleo%20Dental!', text: 'Enviar whatsapp'}
-}
-  };
+        navName: { href: '#', text: 'Núcleo Dental' },
+        navLinks: [
+            { href: '#home', text: 'Inicio' },
+            { href: '#services', text: 'Servicios' },
+            { href: '#specialties', text: 'Especialidades' },
+            { href: '#nosotros', text: 'Nosotros' },
+        ],
+        navBtn: { href: 'https://api.whatsapp.com/send?phone=523329147808&text=%C2%A1Hola%20N%C3%BAcleo%20Dental!', text: 'Enviar whatsapp' }
+    }
+};
