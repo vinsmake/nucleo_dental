7edaad0818548088ddbcd6cce846b68d68066918
vinsmake <ps.enrique.e@gmail.com>
2023-08-27 22:05:48 -0600

    Project updated

---
+++ src/components/services/servicesData.js
@@ -0,0 +1,17 @@
+import icon1 from './servicesList/img/service-icon-1.png'
+import icon2 from './servicesList/img/service-icon-2.png'
+import icon3 from './servicesList/img/service-icon-3.png'
+import icon4 from './servicesList/img/service-icon-4.png'
+import icon5 from './servicesList/img/service-icon-5.png'
+import icon6 from './servicesList/img/service-icon-6.png'
+
+export const servicesData = {
+        servicesList: [
+            { img: icon1, title: 'Periodoncia', text: 'Tratamientos enfocados en las encías y zonas alrededor de los dientes.' },
+            { img: icon2, title: 'Ortodoncia', text: 'Tratamiento enfocado en la aplicación de brackets u otros aparatos para enderezar los dientes.' },
+            { img: icon3, title: 'Prótesis dentales', text: 'Dispositivos soportados por tejidos que remplazan los dientes perdidos.' },
+            { img: icon4, title: 'Blanqueamiento dental', text: 'Limpieza de esmalte que recupera el color original de los dientes.' },
+            { img: icon5, title: 'Pronóstico y prevención', text: 'Chequeo dental enfocado en prevenir posibles danos o problemas a futuro.' },
+            { img: icon6, title: 'Carillas dentales', text: 'Procesos restaurativos que enmascaran la superficie de los dientes para corregir o proteger de posibles danos.' },
+        ],
+};
---
+++ src/components/services/servicesList/Service.jsx
@@ -0,0 +1,31 @@
+import { servicesData } from '../servicesData'
+import './service.css'
+
+
+export const Service = () => {
+
+    const { servicesList } = servicesData;
+
+    return (
+        <>
+
+            {servicesList.map((service, index) => {
+                return (
+                    <article className="service" key={index}>
+
+                    <div className="service__icon">
+                        <img src={service.img} className="service__icon--img" />
+                    </div>
+    
+                    <div className="service__info">
+                        <p className="service__info--title">{service.title}</p>
+                        <p className="service__info--text">{service.text}</p>
+                    </div>
+    
+                </article>
+                )
+            })}
+
+        </>
+    )
+}
