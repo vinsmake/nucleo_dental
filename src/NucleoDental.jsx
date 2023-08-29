16ae44e8158c669c017fd1a9598ae518c80d9d36
vinsmake <ps.enrique.e@gmail.com>
2023-08-28 20:32:06 -0600

    Project updated

--- src/NucleoDental.jsx
+++ src/NucleoDental.jsx
@@ -1,7 +1,7 @@
 import "./normalize.css"
-import { Header } from "./components/header/Header";
 import './NucleoDental.css';
-import { Services } from "./components/services/services";
+import { Header } from "./components/header/Header";
+import { Services } from "./components/services/Services";
 import { Specialties } from "./components/specialties/Specialties";
 
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,5 @@
 import './services.css'
 import banner from './service-banner.png'
-import { Service01 } from './servicesList/Service01'
-import { Service02 } from './servicesList/Service02'
-import { Service03 } from './servicesList/Service03'
-import { Service04 } from './servicesList/Service04'
-import { Service05 } from './servicesList/Service05'
-import { Service06 } from './servicesList/Service06'
 import { Service } from './servicesList/Service'
 
 export const Services = () => {
@@ -15,24 +9,10 @@ export const Services = () => {
             <span className="services--span">Nuestros servicios</span>
             <h2 className="services--title">Con la mejor calidad posible</h2>
 
-        <div className='services__grid'>
-            <div className='services__grid__first'>
+        <div className='services__grid' style={{ backgroundImage: `url(${banner})` }}>
+            <div className='services__grid__services'>
                 <Service/>
-                <Service01/>
-                <Service02/>
-                <Service03/>
             </div>
-            <div className='services__grid__image'>
-                <img className='services__grid__image--img' src={banner}/>
-            </div>
-            <div className='services__grid__last'>
-                <Service04/>
-                <Service05/>
-                <Service06/>
-            </div>
-
-
-
         </div>
 
         </section>
