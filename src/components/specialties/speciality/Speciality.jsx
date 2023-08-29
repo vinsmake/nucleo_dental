9a56084ba5ed66e2fd3c3911f9e7b9e90143b948
vinsmake <ps.enrique.e@gmail.com>
2023-08-28 22:20:20 -0600

    Project updated

--- src/components/specialties/speciality/Speciality.jsx
+++ src/components/specialties/speciality/Speciality.jsx
@@ -7,43 +7,49 @@ export const Speciality = () => {
         <>
 
             {specialitiesData.map((speciality, index) => {
+
+
+                const { position, src, background } = speciality.img
+
                 return (
 
+
+
                     <article className="speciality" key={index}>
-                    <div className={speciality.position}>
-    
-                        <div className='speciality__data__head'>
-                            <span className='speciality__data__head--span'>
-                                {speciality.span}
-                            </span>
-    
-                            <h2 className='speciality__data__head--title'>
-                                {speciality.title}
-                            </h2>
-                        </div>
-    
-                        <div className='speciality__data__body'>
-                            <p className='speciality__data__body--text'>
-                                {speciality.text1}
-                            </p>
-                            <p className='speciality__data__body--text'>
-                                {speciality.text2}
-                            </p>
-    
-    
-    
-                            <a className='speciality__data__body--button' href={speciality.href}>Conoce iTero</a>
-                        </div>
-    
-    
-                        <div className='speciality__data__image'>
-                            <div className='speciality__data__image'>
-                                <img className={`speciality__data__image--img ${speciality.background}`} src={speciality.img}></img>
+                        <div className={position}>
+
+                            <div className='speciality__data__head'>
+                                <span className='speciality__data__head--span'>
+                                    {speciality.span}
+                                </span>
+
+                                <h2 className='speciality__data__head--title'>
+                                    {speciality.title}
+                                </h2>
                             </div>
+
+                            <div className='speciality__data__body'>
+                                <p className='speciality__data__body--text'>
+                                    {speciality.text1}
+                                </p>
+                                <p className='speciality__data__body--text'>
+                                    {speciality.text2}
+                                </p>
+
+
+
+                                <a className='speciality__data__body--button' href={speciality.href}>Conoce iTero</a>
+                            </div>
+
+
+                            <div className='speciality__data__image'>
+                                <div className='speciality__data__image'>
+                                    <img className={`speciality__data__image--img ${background}`} src={src}></img>
+                                </div>
+                            </div>
+
                         </div>
-    
-                    </div>
-                </article>
+                    </article>
                 )
             })}
 
