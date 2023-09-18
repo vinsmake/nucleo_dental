cc91941cf24d4fcfcc4b112d81bfe5bbf337202b
vinsmake <ps.enrique.e@gmail.com>
2023-09-17 21:21:25 -0600

    Project updated

--- src/components/services/servicesList/Modal.jsx
+++ src/components/services/servicesList/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './modal.css'; // Asegúrate de tener este archivo CSS para estilizar el modal
 
 export const Modal = ({ data }) => {
@@ -11,6 +11,22 @@ export const Modal = ({ data }) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+   // Efecto secundario para agregar/quitar la clase al cuerpo del documento
+   useEffect(() => {
+    if (isModalOpen) {
+      // Cuando se abre el modal, agrega una clase para bloquear el desplazamiento
+      document.body.classList.add('body-no-scroll');
+    } else {
+      // Cuando se cierra el modal, quita la clase para permitir el desplazamiento
+      document.body.classList.remove('body-no-scroll');
+    }
+
+    // Limpia la clase cuando el componente se desmonta
+    return () => {
+      document.body.classList.remove('body-no-scroll');
+    };
+  }, [isModalOpen]);
 
   return (
     <div className='modal__container'>
