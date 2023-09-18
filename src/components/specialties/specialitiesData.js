1846434b47d6ed6384cd61ec7685fbd240f4d7f0
vinsmake <ps.enrique.e@gmail.com>
2023-09-18 17:18:36 -0600

    Project updated

--- src/components/specialties/specialitiesData.js
+++ src/components/specialties/specialitiesData.js
@@ -8,6 +8,7 @@ export const specialitiesData = [
         text1: 'iTero es una herramienta eficaz que proporciona visualizaciones muy realistas del aspecto que podría tener tu sonrisa tras el tratamiento.',
         text2: 'iTero es el primer sistema híbrido de imagen dental que registra imágenes 3D en simultáneo sin exposición a radiación, ayudando a detectar caries en las zonas más inaccesibles, descubriendo cambios y lesiones en el esmalte dental, y simulando el resultado de su respectivo tratamiento.',
         href: 'https://www.youtube.com/watch?v=gSPlAW44bXQ&amp;ab_channel=iTeroScanner',
+        btn: 'Conoce iTero',
         img: {
             src: image1,
             position: 'left',
@@ -22,6 +23,7 @@ export const specialitiesData = [
         text1: '¿No te gustan los brackets? Contamos con una opción más discreta: Invisalign.',
         text2: 'Invisalign es una alternativa más estética los brackets, su imagen transparente interfiere menos con tu imagen y permiten tomar el tratamiento sin dejar de mostrar tu sonrisa.',
         href: 'https://api.whatsapp.com/send?phone=523329147808&text=%C2%A1Hola%20N%C3%BAcleo%20Dental!',
+        btn: 'Conoce Invisalign',
         img: {
             src: image2,
             position: 'right',
