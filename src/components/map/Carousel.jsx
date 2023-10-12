b687056aeee47db0535db018e7a214a00f12f141
vinsmake <ps.enrique.e@gmail.com>
2023-10-11 21:14:24 -0600

    Project updated

--- src/components/map/Carousel.jsx
+++ src/components/map/Carousel.jsx
@@ -2,47 +2,45 @@ import './carousel.css'
 
 import React, { useState, useCallback } from 'react';
 
-const Carousel = () => {
-  const [activePos, setActivePos] = useState(0);
+export const Carousel = () => {
+    const [activePos, setActivePos] = useState(0);
 
-  const carouselItems = [
-    { pos: -2, text: '1' },
-    { pos: -1, text: '2' },
-    { pos: 0, text: '3' },
-    { pos: 1, text: '4' },
-    { pos: 2, text: '5' },
-  ];
+    const carouselItems = [
+        { pos: -2, text: '1' },
+        { pos: -1, text: '2' },
+        { pos: 0, text: '3' },
+        { pos: 1, text: '4' },
+        { pos: 2, text: '5' },
+    ];
 
-  const update = useCallback((newActivePos) => {
-    setActivePos(newActivePos);
-  }, []);
+    const update = useCallback((newActivePos) => {
+        setActivePos(newActivePos);
+    }, []);
 
-  const getPos = (current, active) => {
-    const diff = current - active;
+    const getPos = (current, active) => {
+        const diff = current - active;
 
-    if (Math.abs(current - active) > 2) {
-      return -current;
-    }
+        if (Math.abs(current - active) > 2) {
+            return -current;
+        }
 
-    return diff;
-  };
+        return diff;
+    };
 
-  return (
-    <div className="carousel">
-      <ul className="carousel__list">
-        {carouselItems.map((item) => (
-          <li
-            key={item.pos}
-            className={`carousel__item ${item.pos === activePos ? 'carousel__item_active' : ''}`}
-            data-pos={item.pos}
-            onClick={() => update(item.pos)}
-          >
-            {item.text}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="carousel">
+            <ul className="carousel__list">
+                {carouselItems.map((item) => (
+                    <li
+                        key={item.pos}
+                        className={`carousel__item ${item.pos === activePos ? 'carousel__item_active' : ''}`}
+                        data-pos={item.pos}
+                        onClick={() => update(item.pos)}
+                    >
+                        {item.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
-
-export default Carousel;
